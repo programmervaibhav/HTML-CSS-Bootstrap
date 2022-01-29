c9166096744549b394bd2010b56c68518b562be1
programmervaibhav <97295088+programmervaibhav@users.noreply.github.com>
2022-01-29 14:10:09 +0530

    Add files via upload

---
+++ Start something New/resource/javascript/script.js
@@ -0,0 +1,115 @@
+
+/*sticky navigation */
+$(document).ready(function () {
+
+  $('.js--text').waypoint(function (direction) {
+    if (direction == "down") {
+      $('nav').addClass('sticky');
+    } else {
+      $('nav').removeClass('sticky');
+    } {
+      offset:'60px'
+    }
+  });
+
+  /* sticky  navigation
+    var waypoints = $('#handler-first').waypoint(function(direction) {
+    notify(this.element.id + ' hit 25% from top of window') 
+  }, {
+     offset: '25%'
+      })
+  */
+
+
+  /* navigation scroll */
+
+
+  /*https://css-tricks.com/snippets/jquery/smooth-scrolling/*/
+
+  // Select all links with hashes
+  $('a[href*="#"]')
+    // Remove links that don't actually link to anything
+    .not('[href="#"]')
+    .click(function (event) {
+      // On-page links
+      if (
+        location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+        &&
+        location.hostname == this.hostname
+      ) {
+        // Figure out element to scroll to
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        // Does a scroll target exist?
+        if (target.length) {
+          // Only prevent default if animation is actually gonna happen
+          event.preventDefault();
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 1000, function () {
+            // Callback after animation
+            // Must change focus!
+            var $target = $(target);
+            $target.focus();
+            if ($target.is(":focus")) { // Checking if the target was focused
+              return false;
+            } else {
+              $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+              $target.focus(); // Set focus again
+            };
+          });
+        }
+      }
+    });
+
+  /*animation on scroll*/
+
+  $('.js--wp-1').waypoint(function (direction) {
+    $('.js--wp-1').addClass('animated rubberBand');
+  }, {
+    offset: '50%'
+  });
+
+
+  $('.js--wp-2').waypoint(function (direction) {
+    $('.js--wp-2').addClass('animated fadeInUp');
+  }, {
+    offset: '50%'
+  });
+
+
+  $('.js--wp-3').waypoint(function (direction) {
+    $('.js--wp-3').addClass('animated rollIn');
+  }, {
+    offset: '50%'
+  });
+
+
+  $('.js--wp-4').waypoint(function (direction) {
+    $('.js--wp-4').addClass('animated pulse');
+  }, {
+    offset: '50%'
+  });
+
+  $('.js--wp-5').waypoint(function (direction) {
+    $('.js--wp-5').addClass('animated zoomIn');
+  }, {
+    offset: '50%'
+  });
+
+  /*mobile navigation*/
+  $('.js--nav-icon').click(function () {
+    var nav = $('.js--main-nav');
+    var icon = $('.js--nav-icon i');
+
+    nav.slideToggle(200)
+
+    if (icon.hasClass('icofont-navigation-menu')) {
+      icon.addClass('icofont-close');
+      icon.removeClass('icofont-navigation-menu');
+    } else {
+      icon.addClass('icofont-navigation-menu');
+      icon.removeClass('icofont-close');
+    }
+  });
+});
